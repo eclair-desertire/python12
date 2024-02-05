8f553e0868e56ffae244d5f5dd0e250a5ac420d6
zhigerbaj@mail.ru <eclair.desertire@gmail.com>
2024-02-06 00:45:16 +0600

    added first models

---
+++ goods/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from goods.models import Good, Category
+
+admin.site.register(Category)
+admin.site.register(Good)
+# Register your models here.
---
+++ goods/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Category(models.Model):
+    title = models.CharField(verbose_name = 'Наименование категории',max_length=255)
+    
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.title
+
+
+class Good(models.Model):
+    COLORS_CHOICES = (
+        ('red','Красное'),
+        ('white','Белое'),
+    )
+
+    image = models.ImageField(verbose_name='Изображение')
+    category = models.ForeignKey(Category,verbose_name = 'Категория', null = True, blank = True, on_delete = models.CASCADE)
+    color = models.CharField(verbose_name = 'Цвет', choices = COLORS_CHOICES, max_length = 255, default = 'red')
+    title = models.CharField(max_length = 255, verbose_name = 'Заголовок')
+    price = models.IntegerField(default = 0, verbose_name = 'Цена')
+    created_at = models.DateTimeField(verbose_name = 'Дата создания', default = timezone.now)
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+    
+    def __str__(self) -> str:
+        return f'{self.pk} - {self.title}'
+# Create your models here.
