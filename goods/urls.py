8d7f84a3cff73f5f3cc15e9132c2dcc28873603e
zhigerbaj@mail.ru <eclair.desertire@gmail.com>
2024-02-19 20:12:46 +0600

    .

--- goods/urls.py
+++ goods/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from goods.views import goods_list, good_detail
+from goods.views import goods_list, good_detail, good_new, good_edit
 
 urlpatterns = [
     path('',goods_list, name='goods_list'),
-    path('<str:title>/detail/',good_detail, name='good_detail')
+    path('<int:pk>/detail/',good_detail, name='good_detail'),
+    path('<int:pk>/edit/',good_edit ,name = 'good_edit'),
+    path('new/',good_new,name='good_new'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from goods.models import Good
+from goods.forms import GoodForm
 
 def goods_list(request):
     goods = Good.objects.all()
@@ -11,3 +12,29 @@ def good_detail(request, pk):
     good = get_object_or_404(Good.objects.all(),pk=pk)
     # Good.objects.get(pk=pk)
     return render(request, 'goods/good_detail.html',{'good':good})
+
+
+def good_new(request):
+    if request.method == "POST":
+        form = GoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            good = form.instance
+            return redirect('good_detail', pk=good.pk)
+    else:
+        form = GoodForm()
+    return render(request, 'goods/good_new.html',{'form':form})
+
+
+def good_edit(request, pk):
+    good = get_object_or_404(Good.objects.all(),pk=pk)
+
+    if request.method == "POST":
+        form = GoodForm(request.POST, request.FILES, instance=good)
+        if form.is_valid():
+            form.save()
+            good = form.instance
+            return redirect('good_detail', pk=good.pk)
+    else:
+        form = GoodForm(instance=good)
+    return render(request, 'goods/good_edit.html',{'form':form, 'good':good})
