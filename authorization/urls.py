6a540e2fc75dc68bcfb3849f796da3be103f7952
zhigerbaj@mail.ru <eclair.desertire@gmail.com>
2024-02-26 20:44:34 +0600

    register endpnt

--- authorization/urls.py
+++ authorization/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from authorization.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    UserRegisterView
 )
 
 urlpatterns = [
     path('token/',TokenObtainPairView.as_view()),
     path('token/refresh/',TokenRefreshView.as_view()),
+    path('register/', UserRegisterView.as_view()),
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -3,6 +3,12 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from authorization.models import User
+from django.contrib.auth.hashers import make_password
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.permissions import (
     AllowAny,
 )
@@ -16,3 +22,33 @@ class TokenObtainPairView(TokenObtainPairView):
 class TokenRefreshView(TokenRefreshView):
     permission_classes = [AllowAny]
     serializer_class = TokenRefreshSerializer
+
+
+'''{
+    "username":"alisher",
+    "password":"123321",
+    "first_name":"Alisher",
+    "email":"info@mail.kz",
+    "role":2
+}'''
+
+class UserRegisterView(APIView):
+
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        first_name = request.data['first_name']
+        email = request.data['email']
+        role = request.data['role']
+        try:
+            get_object_or_404(User.objects.all(),username=username)
+            return Response(data={'info':'Пользователь уже существует'},status=status.HTTP_409_CONFLICT)
+        except:
+            User.objects.create(
+                username=username, 
+                password=make_password(password),
+                first_name=first_name,
+                email=email,
+                role=role)
+            return Response(status=status.HTTP_200_OK)
+        
